ed8bb9bb7f605f352926074d610321ad0d330ae7
brendanjohnharris <bhar9988@uni.sydney.edu.au>
2024-11-06 15:14:37 +1100

    Fix build

--- PKGBUILD
+++ PKGBUILD
@@ -1,15 +1,15 @@
 # Maintainer: Brendan Harris <brendanjohnharris@gmail.com>
-pkgname=svg2csv
-pkgver=1.0.0
+pkgname=svg2tsv
+pkgver=1.0.1
 pkgrel=1
-pkgdesc="A command-line tool to extract points from SVG paths and save them as a matrix."
+pkgdesc="A command-line tool to extract points from SVG paths and save them as a TSV matrix."
 arch=('any')
 url="https://github.com/brendanjohnharris/svg2tsv"
 license=('MIT')
-depends=('python' 'python-svgpathtools' 'python-numpy')
+depends=('python' 'python-svgpathtools' 'python-numpy' 'python-svgwrite')
 makedepends=('python-setuptools')
-source=("$pkgname-$pkgver.tar.gz::$url/archive/v$pkgver.tar.gz")
-sha256sums=('SKIP')
+source=("$pkgname-$pkgver.tar.gz::https://github.com/brendanjohnharris/svg2tsv/archive/refs/tags/v$pkgver.tar.gz")
+sha256sums=('SKIP') # Replace 'SKIP' with the actual checksum for security
 
 build() {
     cd "$srcdir/$pkgname-$pkgver"
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="svg2tsv",
+    version="1.0.1",
+    description="A command-line tool to extract points from SVG paths and save them as a TSV matrix.",
+    author="Brendan Harris",
+    author_email="brendanjohnharris@gmail.com",
+    url="https://github.com/brendanjohnharris/svg2tsv",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "svgpathtools",
+        "numpy",
+        "svgwrite",
+    ],
+    entry_points={
+        "console_scripts": [
+            "svg2tsv=svg2tsv:main",  # Ensure svg2tsv.py has a main() function
+            "tsv2svg=tsv2svg:main",  # Ensure tsv2svg.py has a main() function
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
